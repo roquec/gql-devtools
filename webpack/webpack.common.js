0171da433db840ef8ccb40da7cecd06adc6ac806
RoqueC <roquecarrizof@gmail.com>
2021-02-15 01:31:40 +0100

    VSCode config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,14 +2,6 @@ import path from "path";
 const __dirname = path.resolve();
 const srcDir = path.join(__dirname, "src");
 
-// export const entry = {
-//   entry: join(srcDir, "panel.js"),
-// };
-// export const output = {
-//   path: join(__dirname, "../dist/js"),
-//   filename: "[name].js",
-// };
-
 export default {
   entry: {
     panel: path.join(srcDir, "js/panel.js"),
@@ -23,27 +15,3 @@ export default {
     modules: [path.join(__dirname, "node_modules")],
   },
 };
-// export const optimization = {
-//     splitChunks: {
-//         name: "vendor",
-//         chunks: "initial",
-//     },
-// };
-// export const module = {
-//     rules: [
-//         {
-//             test: /\.tsx?$/,
-//             use: "ts-loader",
-//             exclude: /node_modules/,
-//         },
-//     ],
-// };
-// export const resolve = {
-//     extensions: [".ts", ".tsx", ".js"],
-// };
-// export const plugins = [
-//     new CopyPlugin({
-//         patterns: [{ from: ".", to: "../", context: "public" }],
-//         options: {},
-//     }),
-// ];
